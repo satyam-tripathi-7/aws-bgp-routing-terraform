4bc48aa583dd41fe75afb6d96d8479256b718b54
satyam-tripathi-7 <satyamtripathi40@gmail.com>
2026-04-27 01:57:40 +0530

    Remove redundant region argument from VPC modules

--- network/vpc.tf
+++ network/vpc.tf
@@ -4,7 +4,6 @@ module "vpc1" {
 
   name = "vpc1"
   cidr = "10.0.0.0/16"
-  region = var.region
 
   azs             = ["ap-south-1a",]
   private_subnets = ["10.0.1.0/24", ]
@@ -26,7 +25,6 @@ module "vpc2" {
 
   name = "vpc2"
   cidr = "10.1.0.0/16"
-  region = var.region
 
   azs             = ["ap-south-1a"]
   private_subnets = ["10.1.1.0/24"]
